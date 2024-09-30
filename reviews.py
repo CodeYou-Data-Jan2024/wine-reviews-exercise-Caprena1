f40bd78ce3cb2aafec5353401dfd710b892e7727
Caprena1 <robcaprena@gmail.com>
2024-09-30 17:22:52 -0400

    added country and count columns

--- reviews.py
+++ reviews.py
@@ -1,2 +1,17 @@
-# add your code here
+import pandas as pd
+
+reviews = pd.read_csv('data/winemag-data-130k-v2.csv', index_col=0)
+
+#count = reviews.rename(columns={'description' : 'count'}).sum()
+
+counts = reviews[['country']].value_counts()#.rename(columns={'description' : 'count'}).sum()
+
+counts_reviews = counts.to_frame().reset_index()
+counts_reviews.columns = ['country', 'count']
+#points = counts_reviews.groupby(['country', 'count', 'points'])
+
+#counts_reviews = points.iloc['points']
+
+
+print(counts_reviews)
 
